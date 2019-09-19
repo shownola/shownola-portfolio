d68440c69ef3ae140d1aa397f0dcacce8225c3f4
shownola <swasieko@gmail.com>
2019-09-19 16:20:30 -0700

    Add skills model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_194044) do
+ActiveRecord::Schema.define(version: 2019_09_19_225859) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_used"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
