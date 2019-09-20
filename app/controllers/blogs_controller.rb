cfc00219ca89352c4c1c171b68718ffede3e984d
shownola <swasieko@gmail.com>
2019-09-20 14:17:20 -0700

    Integrate permalinks with friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -47,10 +47,15 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
 end
+
+# If you're adding FriendlyId to an existing app and need
+# to generate slugs for existing users, do this from the
+# console, runner, or add a Rake task:
+# User.find_each(&:save)
