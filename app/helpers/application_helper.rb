21d2191ecd1b4cfbc5f9e36c94d048745e9987c0
shownola <swasieko@gmail.com>
2019-12-31 13:09:36 -0700

    Implement HEREDOC for multiple navigation layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,10 +13,18 @@ module ApplicationHelper
   def source_helper(layout_name)
      if session[:source]
        greeting = "Thanks for visiting my site from #{session[:source]} and you are on the #{layout_name} page"
-       content_tag(:p,greeting, class: 'source-greeting')
+       content_tag(:p, greeting, class: 'source-greeting')
      end
   end
 
   def nav_helper(style, tag_type)
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_path}" class="#{style}">About</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style}">Portfolios</a></#{tag_type}>
+NAV
+    nav_links.html_safe
   end
 end
