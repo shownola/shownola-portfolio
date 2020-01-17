3319fd2e4cca7303911fe997e32b06b275d80ac1
shownola <swasieko@gmail.com>
2020-01-17 12:56:34 -0700

    Add url to portfolios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_003520) do
+ActiveRecord::Schema.define(version: 2020_01_17_171229) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_01_10_003520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
+    t.string "url"
   end
 
   create_table "skills", force: :cascade do |t|
