3ac5d8bccfb287d1bc37c38edc8f6646e49d6257
shownola <swasieko@gmail.com>
2020-08-19 19:59:00 -0700

    Removed error routes

--- config/routes.rb
+++ config/routes.rb
@@ -29,11 +29,11 @@ resources :portfolios
 
 
 
-  match "/404", to: "errors#not_found", via: :all
-  match "/422", to: "errors#unacceptable", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
-
-  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
+  # match "/404", to: "errors#not_found", via: :all
+  # match "/422", to: "errors#unacceptable", via: :all
+  # match "/500", to: "errors#internal_server_error", via: :all
+  #
+  # match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
 
 
 end
